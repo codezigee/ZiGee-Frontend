429a1a181eff39ce9717af363629d92d883d7c10
H <instantseaside@gmail.com>
2025-06-04 20:19:17 +0900

    [Feat] 기능별 빈 화면 추가 #1

---
+++ zigee_app/lib/features/auth/signup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen
+    extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('회원가입 화면'),
+      ),
+    );
+  }
+}
