0859a07f497d72c6ef9c0c088a5c46be9ed6b9c9
H <instantseaside@gmail.com>
2025-06-04 20:23:20 +0900

    [Feat] 기능별 화면 라우트 등록 #1

---
+++ zigee_app/lib/app/routes/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const initial = '/login';
+  static const booking = '/booking';
+  static const myBooking =
+      '/my-booking';
+  static const settings = '/settings';
+}
