fce32e526b5d2c437e38a3bb66bd59c7211cf5ae
H <instantseaside@gmail.com>
2025-07-14 23:05:20 +0900

    [Feat] 카카오 SDK 초기화 및 로그인 기능 추가 #9

--- zigee_app/lib/main.dart
+++ zigee_app/lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:zigee_app/app/app.dart';
+import 'package:zigee_app/app/config/env.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  KakaoSdk.init(
+    nativeAppKey: '65649053097626958171b85d2ad5251e',
+    javaScriptAppKey: Env.kakaoJavascriptKey,
+  );
+
+  initialize();
+
   runApp(const ZigeeApp());
+}
+
+void initialize() async {
+  final result = await UserApi.instance.loginWithKakaoAccount();
 }
