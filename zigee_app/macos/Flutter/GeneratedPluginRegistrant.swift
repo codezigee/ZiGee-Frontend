56c87ce44c8d03bd09a38ccc6a2035f99f000cf1
H <instantseaside@gmail.com>
2025-08-30 00:19:52 +0900

    [Feat] sign_in_with_apple 의존성 추가 #21

--- zigee_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ zigee_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import path_provider_foundation
 import shared_preferences_foundation
+import sign_in_with_apple
 import sqflite_darwin
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
