06a2e6ed7121224f9e940c7d35066ff8b037d6e0
H <instantseaside@gmail.com>
2025-09-01 21:35:30 +0900

    [Refactor] AuthSuccess에서 OAuthToken을 AuthToken으로 타입 변경 및 providerType 필드 추가 #20

--- zigee_app/lib/features/auth/models/auth_result.dart
+++ zigee_app/lib/features/auth/models/auth_result.dart
@@ -1,29 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:zigee_app/features/auth/models/auth_provider_type.dart';
+import 'package:zigee_app/features/auth/models/auth_token.dart';
 
 sealed class AuthResult {}
 
 class AuthSuccess extends AuthResult {
-  // TODO: - OAuthToken 대신 AppToken으로 변경 필요(서버에서 발급한 JWT 형식으로)
-  // 현재는 카카오 OAuthToken을 임시로 사용중
-  final OAuthToken token;
+  final AuthToken token;
+  final AuthProviderType providerType;
 
-  AuthSuccess(this.token) {
-    debugPrint('[로그인 성공]');
-  }
+  AuthSuccess({required this.token, required this.providerType});
 }
 
 class AuthFailure extends AuthResult {
   final AuthError error;
-  AuthFailure(this.error) {
-    debugPrint('[로그인 실패] ${error.message}');
-  }
+
+  AuthFailure(this.error);
 }
 
 class AuthCancelled extends AuthResult {
-  AuthCancelled() {
-    debugPrint('[로그인 취소]');
-  }
+  AuthCancelled();
 }
 
 enum AuthErrorType {
